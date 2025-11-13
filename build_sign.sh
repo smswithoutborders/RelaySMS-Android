0ff1e787548349925145acf6a13ff778d27b8c96
sherlock <wisdomnji@gmail.com>
2025-11-13 15:33:15 +0100

    update: running venv

--- build_sign.sh
+++ build_sign.sh
@@ -2,14 +2,13 @@
 set -euo pipefail
 
 MIN_SDK=24
-github_url="https://api.github.com/repos/smswithoutborders/SMSWithoutBorders-App-Android/releases"
+github_url="https://api.github.com/repos/dekusms/DekuSMS-Android/releases"
 
 python3 -m venv venv
-{
-    source venv/bin/activate
-    pip install -r requirements.txt
-    ./bump_version.py "$(git symbolic-ref HEAD)"
-}
+venv/bin/pip install -r requirements.txt
+
+# use venv's python explicitly for all python commands
+venv/bin/python bump_version.py "$(git symbolic-ref HEAD)"
 
 tagVersion=$(sed -n '5p' version.properties | cut -d "=" -f 2)
 label=$(sed -n '4p' version.properties | cut -d "=" -f 2)
@@ -32,7 +31,6 @@ apksigner sign --ks app/keys/app-release-key.jks \
 
 # This will now stop the script immediately if diffoscope fails
 diffoscope apk-outputs/"$label".apk app/build/outputs/apk/release/"$label".apk
-rm apk-outputs/"$label".apk
 
 ./gradlew assemble bundleRelease
 apksigner sign --ks app/keys/app-release-key.jks \
@@ -44,20 +42,16 @@ apksigner sign --ks app/keys/app-release-key.jks \
 git push origin "$branch"
 git push --tag
 
-python3 -m venv venv
-{
-    source venv/bin/activate
-    pip install -r requirements.txt
-    python3 release.py \
-      --version_code "${tagVersion}" \
-      --version_name "${label}" \
-      --description "<b>Release</b>: ${label}<br><b>Build No</b>: ${tagVersion}<br><b>shasum</b>: $(shasum apk-outputs/${label}.apk)" \
-      --branch "${branch}" \
-      --track "${track}" \
-      --app_bundle_file app/build/outputs/bundle/release/app-bundle.aab \
-      --app_apk_file app/build/outputs/apk/release/"${label}".apk \
-      --status "completed" \
-      --platforms "all" \
-      --github_url "${github_url}"
-}
+venv/bin/python release.py \
+	--version_code "${tagVersion}" \
+        --version_name "${label}" \
+        --description "<b>Release</b>: ${label}<br><b>Build No</b>: ${tagVersion}<br><b>shasum</b>: $(shasum apk-outputs/${label}.apk)" \
+        --branch "${branch}" \
+        --track "${track}" \
+        --app_bundle_file app/build/outputs/bundle/release/app-bundle.aab \
+        --app_apk_file app/build/outputs/apk/release/"${label}".apk \
+        --status "completed" \
+        --platforms "all" \
+        --github_url "${github_url}"
 
+rm apk-outputs/"$label".apk
