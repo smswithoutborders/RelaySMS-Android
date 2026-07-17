3f854029da50033945ee9c3768f46a6adf42ded3
Mildrette <mildredfonka1@gmail.com>
2026-07-17 10:40:58 +0100

    Update the home screen to match new home screen design

--- app/src/main/java/com/example/sw0b_001/ui/views/WelcomeView.kt
+++ app/src/main/java/com/example/sw0b_001/ui/views/WelcomeView.kt
@@ -61,7 +61,7 @@ fun WelcomeMainView() {
                 text = "Send messages online even when the internet is gone.",
                 textAlign = TextAlign.Center,
                 style = MaterialTheme.typography.headlineSmall.copy(
-                    fontWeight = FontWeight.Bold
+                    fontWeight = FontWeight.SemiBold
                 ),
                 modifier = Modifier.fillMaxWidth()
             )
